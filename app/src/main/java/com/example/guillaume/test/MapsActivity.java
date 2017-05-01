4f1183905241df1e68ad0a448554ea93adf7835b
Guillaume <guillaumeval.techer@gmail.com>
2017-05-01 21:03:09 +0200

    test

--- app/src/main/java/com/example/guillaume/test/MapsActivity.java
+++ app/src/main/java/com/example/guillaume/test/MapsActivity.java
@@ -2,17 +2,26 @@ package com.example.guillaume.test;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.common.api.Status;
+
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, PlaceSelectionListener  {
 
     private GoogleMap mMap;
+    LatLng location = new LatLng(0,0);
+    Marker marker = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +46,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment) getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+        autocompleteFragment.setOnPlaceSelectedListener(this);
 
-        // Add a marker in Sydney and move the camera
-        LatLng PMC = new LatLng(47.492762, -0.551592);
-        mMap.addMarker(new MarkerOptions().position(PMC).title("Park My Car"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(PMC));
+    }
+
+    @Override
+    public void onPlaceSelected(Place place) {
+        if (marker != null){
+            marker.remove();
+        }
+        MapsActivity.this.location =  place.getLatLng();
+        Log.i("¨Place_auto", "Place: " + place.getName());
+        marker = mMap.addMarker(new MarkerOptions().position(location).title("Park My Car"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+    }
+
+    @Override
+    public void onError(Status status) {
+        Log.i("¨Place_auto", "kaput");
     }
 }
